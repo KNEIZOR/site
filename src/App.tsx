9074c125c04dd42cbebda735ff8f84424cea3b30
Kneizor <denisstukalo33@gmail.com>
2025-06-30 05:01:23 +0300

    add routing and async pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Counter } from "./components/Counter";
-import "./index.scss"
+import "./index.scss";
+import { Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 
 const App = () => {
     return (
         <div className="app">
-            asadasd 
-            <Counter />
+            <Link to={"/"}>Главная</Link>
+            <Link to={"/about"}>О нас</Link>
+
+            <Suspense fallback={<div>...Loading</div>}>
+                <Routes>
+                    <Route path="/about" element={<AboutPageAsync />} />
+                    <Route path="/" element={<MainPageAsync />} />
+                </Routes>
+            </Suspense> 
         </div>
     );
 };
