592538c7b2b7f16af6a78371a82d6e8b6e395084
Kneizor <denisstukalo33@gmail.com>
2025-06-30 05:54:20 +0300

    add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
-import React, { Suspense } from "react";
-import { Counter } from "./components/Counter";
-import "./index.scss";
+import React, { Suspense, useContext, useState } from "react";
+import "./styles/index.scss";
 import { Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { useTheme } from "./theme/useTheme";
 
 const App = () => {
+    const { theme, toggleTheme } = useTheme();
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
             <Link to={"/"}>Главная</Link>
             <Link to={"/about"}>О нас</Link>
+
+            <button onClick={toggleTheme}>Тема</button>
 
             <Suspense fallback={<div>...Loading</div>}>
                 <Routes>
                     <Route path="/about" element={<AboutPageAsync />} />
                     <Route path="/" element={<MainPageAsync />} />
                 </Routes>
-            </Suspense> 
+            </Suspense>
         </div>
     );
 };
