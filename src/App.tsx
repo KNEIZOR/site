57d746acdfce46a638c9469026931027f66444b8
Kneizor <denisstukalo33@gmail.com>
2025-07-01 04:57:21 +0300

    add classNames function

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,15 @@ import { Link } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
 
 const App = () => {
     const { theme, toggleTheme } = useTheme();
-
+    classNames("remove-btn", { hovered: true, selectable: true, red: true }, [
+        "pdg",
+    ]);
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames("app", {}, [theme])}>
             <Link to={"/"}>Главная</Link>
             <Link to={"/about"}>О нас</Link>
 
