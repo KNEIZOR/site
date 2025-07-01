1478e91b76b775e89c074a008bf690c8e7dc2c7c
Kneizor <denisstukalo33@gmail.com>
2025-07-01 05:45:11 +0300

    add absolute path and methodology feature sliced design

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import App from "./app/App";
 import { BrowserRouter } from "react-router-dom";
-import ThemeProvider from "./theme/ThemeProvider";
+import { ThemeProvider } from "app/providers/themeProvider";
+
 
 const domNode = document.getElementById("root");
 const root = createRoot(domNode);
