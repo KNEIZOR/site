93a86b27ee6bad1a0c37ff625bebd21aa06d2a31
Kneizor <denisstukalo33@gmail.com>
2026-01-16 10:28:47 +0300

    fix pluginBundleAnalyzer, fix lofinForm stories

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -22,12 +22,6 @@ export function buildPlugins({
         }),
     ];
 
-    plugins.push(
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false,
-        }),
-    );
-
     if (isDev) {
         plugins.push(new webpack.HotModuleReplacementPlugin());
         plugins.push(
